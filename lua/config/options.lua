029c96e91401db21d6b0abebf36b3e3bbdddc52e
David <dav1dch@outlook.com>
2024-05-28 12:33:48 +0800

    color

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,5 @@
 -- Add any additional options here
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 vim.g.python3_host_prog = "/usr/bin/python3"
+
+vim.opt.termguicolors = true
